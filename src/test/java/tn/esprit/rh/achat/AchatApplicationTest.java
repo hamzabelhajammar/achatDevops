4371fd48247dd8fdeb806954d9928e04953fd0de
hamzabelhajammar <73438148+hamzabelhajammar@users.noreply.github.com>
2022-11-09 00:56:03 +0100

    test unitaire

---
+++ src/test/java/tn/esprit/rh/achat/AchatApplicationTest.java
@@ -0,0 +1,16 @@
+package tn.esprit.rh.achat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+public class AchatApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
